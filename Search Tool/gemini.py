112976cb6cd5bbd051fd5fff309fac4beb72cb72
sweprem <157076269+sweprem@users.noreply.github.com>
2025-03-24 19:29:27 -0500

    Update gemini.py

--- Search Tool/gemini.py
+++ Search Tool/gemini.py
@@ -4,7 +4,7 @@
 
 from google import genai
 
-client = genai.Client(api_key="AIzaSyAmN7zbJiyjx8n6KXm7ocmXyqSXsyxHdZA")
+client = genai.Client(api_key="")
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works"
 )
